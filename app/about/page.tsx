8f45a9e295c4502268333dcd36509f079e7a4504
martinamancuso <martinamancuso24@gmail.com>
2024-05-22 12:43:42 +0200

    feat(about-me): create "introduction" section and start styling it

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,48 @@
+import Image from "next/image";
+import MartinaMancusoPhoto from "./MartinaMancusoPhoto.jpg";
+
 export default function About() {
-  return <h3>About Page</h3>;
+  return (
+    <>
+      <div className="flex flex-col h-full w-full">
+        <p>About Me</p>
+        <Image
+          src={MartinaMancusoPhoto}
+          alt="martina mancuso photo"
+          width={300}
+          height={200}
+        />
+        <p>Martina Mancuso</p>
+        <p>Junior Full-Stack Developer</p>
+        <hr />
+        <p className="text-justify">
+          <p>
+            «As you leave for Ithaca I hope your road is long, full of
+            adventures, full of discovery.»
+          </p>
+          <p>
+            I choose to give a turn in my life, devoting myself to the study of
+            web development after having worked for almost 5 years in the world
+            of customer service.
+          </p>
+          <p>
+            I start this adventure with Develhope, taking the first steps
+            towards JavaScript, HTML, CSS, React, TypeScript, SQL, Node.js, step
+            by step, pixel by pixel.
+          </p>
+          <p>
+            Thanks to Develhope and my previous experience, I have developed the
+            ability to work in teams, to look for creative solutions together
+            with my colleagues and to focus tenaciously on the goals we have
+            set, in the short and long term.
+          </p>
+          <p>
+            I am passionate about Communication and Psychology and I believe in
+            "please" and "thank you".
+          </p>
+        </p>
+        <hr />
+      </div>
+    </>
+  );
 }
