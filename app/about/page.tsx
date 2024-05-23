3306e4c5bd7691f315cd81104e51cabcc2216f9f
martinamancuso <martinamancuso24@gmail.com>
2024-05-23 17:25:20 +0200

    feat: add flexbox, flex-col, and gap styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -47,31 +47,27 @@ export default function About() {
           <p className="font-medium">Junior Full-Stack Developer</p>
         </div>
         <hr />
-        <div className="text-justify py-6">
+        <div className="text-justify py-6 flex flex-col gap-5">
           <p>
             «As you leave for Ithaca I hope your road is long, full of
             adventures, full of discovery.»
           </p>
-          <br />
           <p>
             I choose to give a turn in my life, devoting myself to the study of
             web development after having worked for almost 5 years in the world
             of customer service.
           </p>
-          <br />
           <p>
             I start this adventure with Develhope, taking the first steps
             towards JavaScript, HTML, CSS, React, TypeScript, SQL, Node.js, step
             by step, pixel by pixel.
           </p>
-          <br />
           <p>
             Thanks to Develhope and my previous experience, I have developed the
             ability to work in teams, to look for creative solutions together
             with my colleagues and to focus tenaciously on the goals we have
             set, in the short and long term.
           </p>
-          <br />
           <p>
             I am passionate about Communication and Psychology and I believe in
             "please" and "thank you".
