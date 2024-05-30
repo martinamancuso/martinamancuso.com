d1fa38639316cd4486f1ee19893ff2ce18759c1f
martinamancuso <martinamancuso24@gmail.com>
2024-05-30 17:35:59 +0200

    style(container): replaced flex with margin: 0 auto

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <body className={`${poppins.className}`}>
           <div className="lg:flex lg:w-screen lg:h-screen">
             <Sidebar />
-            <main className="pt-24 pb-12 flex-grow overflow-auto flex flex-col items-center">
+            <main className="pt-24 pb-12 flex-grow overflow-auto">
               {children}
             </main>
           </div>
