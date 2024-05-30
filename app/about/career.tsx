6aacb1528d3ed94057f56638e110a507991c73a1
martinamancuso <martinamancuso24@gmail.com>
2024-05-30 15:54:06 +0200

    fix(css): remove unnecessary padding

--- app/about/career.tsx
+++ app/about/career.tsx
@@ -77,7 +77,7 @@ export function Career() {
         </div>
       </div>
       <div>
-        <p className="text-xl font-bold mb-10 lg:pt-0">Education</p>
+        <p className="text-xl font-bold mb-10">Education</p>
         <div className="mb-10">
           <p className="font-semibold text-base pb-2">Develhope</p>
           {renderList(develhopeEducation)}
