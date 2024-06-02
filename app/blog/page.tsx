58936571b0c1ee190158ad479029b4b57ee86dab
martinamancuso <martinamancuso24@gmail.com>
2024-06-02 10:46:17 +0200

    fix: blog cards list layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,11 @@
 import { BlogCard } from "./blog-card";
+import style from "./page.module.scss";
 
 export default function Blog() {
   return (
     <div className="container">
       <p className="font-extrabold text-3xl mb-16">Blog</p>
-      <div className="flex flex-col gap-12 lg:flex-row lg:flex-wrap">
+      <div className={`flex flex-col gap-y-12 gap-x-12 lg:flex-row lg:flex-wrap ${style.container}`}>
         <BlogCard />
         <BlogCard />
         <BlogCard />
