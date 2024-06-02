4fa1d5b3c3cbb038a57580deb21f3a3c9d05a86c
martinamancuso <martinamancuso24@gmail.com>
2024-06-02 11:05:40 +0200

    feat: remove image component

--- app/blog/blog-card.tsx
+++ app/blog/blog-card.tsx
@@ -1,19 +1,20 @@
-import Image from "next/image";
-
-export function BlogCard({ date, title }: { date: string; title: string}) {
+export function BlogCard({
+  image,
+  date,
+  title,
+}: {
+  image: string;
+  date: string;
+  title: string;
+}) {
   return (
     <div className="bg-white shadow grow shrink-0 basis-2/5">
-      {/* <div>
-        <Image
-          src="https://cdn.britannica.com/36/234736-050-4AC5B6D5/Scottish-fold-cat.jpg"
-          alt="scottish cat"
-          width={500}
-          height={300}
-        />
-      </div> */}
+      <img src={image} alt={title} className="w-full h-64 object-cover" />
       <div className="p-7">
         <div className="pb-2 text-sm text-greyText">{date}</div>
-        <div className="pt-2 border-t border-greyBorders text-lg font-semibold">{title}</div>
+        <div className="pt-2 border-t border-greyBorders text-lg font-semibold">
+          {title}
+        </div>
       </div>
     </div>
   );
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,12 +5,34 @@ export default function Blog() {
   return (
     <div className="container">
       <p className="font-extrabold text-3xl mb-16">Blog</p>
-      <div className={`flex flex-col gap-y-12 gap-x-12 lg:flex-row lg:flex-wrap ${style.container}`}>
-        <BlogCard date="24 Jun 2024" title="Lorem Ipsum" />
-        <BlogCard date="24 Jun 2024" title="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum" />
-        <BlogCard date="24 Jun 2024" title="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum" />
-        <BlogCard date="24 Jun 2024" title="Lorem Ipsum" />
-        <BlogCard date="24 Jun 2024" title="Lorem Ipsum" />
+      <div
+        className={`flex flex-col gap-y-12 gap-x-12 lg:flex-row lg:flex-wrap ${style.container}`}
+      >
+        <BlogCard
+          image="https://picsum.photos/400/600"
+          date="24 Jun 2024"
+          title="Lorem Ipsum"
+        />
+        <BlogCard
+          image="https://picsum.photos/600/400"
+          date="24 Jun 2024"
+          title="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+        />
+        <BlogCard
+          image="https://picsum.photos/800/1000"
+          date="24 Jun 2024"
+          title="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+        />
+        <BlogCard
+          image="https://picsum.photos/1000/800"
+          date="24 Jun 2024"
+          title="Lorem Ipsum"
+        />
+        <BlogCard
+          image="https://picsum.photos/1000/1000"
+          date="24 Jun 2024"
+          title="Lorem Ipsum"
+        />
       </div>
     </div>
   );
