95dca130603c6c6980dce88323067fb25f929495
martinamancuso <martinamancuso24@gmail.com>
2024-06-02 18:26:32 +0200

    feat: linked blog cards to post pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,26 +10,31 @@ export default function Blog() {
         className={`flex flex-col gap-12 lg:flex-row lg:flex-wrap ${style.container}`}
       >
         <BlogCard
+          path="/post"
           image="https://picsum.photos/400/600"
           date="24 Jun 2024"
           title="Lorem Ipsum"
         />
         <BlogCard
+          path="/post"
           image="https://picsum.photos/600/400"
           date="24 Jun 2024"
           title="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
         />
         <BlogCard
+          path="/post"
           image="https://picsum.photos/800/1000"
           date="24 Jun 2024"
           title="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
         />
         <BlogCard
+          path="/post"
           image="https://picsum.photos/1000/800"
           date="24 Jun 2024"
           title="Lorem Ipsum"
         />
         <BlogCard
+          path="/post"
           image="https://picsum.photos/1000/1000"
           date="24 Jun 2024"
           title="Lorem Ipsum"
