1539ebbfb1886a03ba9e0ddbb0c9a49657ce424c
martinamancuso <martinamancuso24@gmail.com>
2024-06-04 10:14:03 +0200

    style: modified font-size

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -8,7 +8,7 @@ export default function Post() {
       />
       <div>
         <div className="pb-3 text-sm text-greyText">24 Jun 2024</div>
-        <div className="py-9 border-t border-greyBorders text-xl font-semibold">
+        <div className="py-9 border-t border-greyBorders text-2xl font-semibold">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </div>
         <div className="text-sm text-greyText tracking-wide leading-loose text-justify flex flex-col gap-5">
