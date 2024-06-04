814b8471979375a873f3e2a5c5fbdc1a8f7f0815
martinamancuso <martinamancuso24@gmail.com>
2024-06-04 18:38:31 +0200

    feat: added a typeform to the "contact me" button in the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,14 @@ import Image from "next/image";
 import MartinaMancusoSquare from "./martina-mancuso-square.jpg";
 
 export default function Home() {
-  // lg:size-full lg:flex lg:justify-around
+  function handleContactForm() {
+    const link = document.createElement("a");
+    link.href = "https://fvxbhxqrrxf.typeform.com/to/BZ3YKWsW";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
   return (
     <div className="container pt-20 lg:pt-0 flex flex-col lg:flex-row items-center lg:h-full">
       <div className="flex flex-col-reverse gap-6 lg:gap-0 lg:flex-row lg:items-center lg:w-full">
@@ -18,7 +25,7 @@ export default function Home() {
             <p>Based in Italy</p>
           </div>
           <div className="pt-9 lg:pt-11 text-center lg:text-left">
-            <Button onClick={() => {}}>Contact me</Button>
+            <Button onClick={handleContactForm}>Contact me</Button>
           </div>
         </div>
         <div>
