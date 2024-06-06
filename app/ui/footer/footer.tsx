f991152b5b636cb6eb3c1f2f2f14e300bc7de11c
martinamancuso <martinamancuso24@gmail.com>
2024-06-06 16:50:49 +0200

    feat(footer): add "made with love" message

--- app/ui/footer/footer.tsx
+++ app/ui/footer/footer.tsx
@@ -4,21 +4,40 @@ import linkedinIcon from "./social-icons/linkedinIcon.png";
 
 export function Footer() {
   return (
-    <div className="hidden lg:flex">
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.linkedin.com/in/martinamancuso/"
-      >
-        <Image src={linkedinIcon} alt="linkedin logo" className="mr-3 hover:opacity-70" />
-      </a>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/martinamancuso"
-      >
-        <Image src={githubIcon} alt="github logo" className="hover:opacity-70"/>
-      </a>
-    </div>
+    <>
+      <div className="hidden lg:flex lg:flex-col lg:gap-3">
+        <div className="lg:flex lg:flex-row lg:justify-center">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/martinamancuso/"
+          >
+            <Image
+              src={linkedinIcon}
+              alt="linkedin logo"
+              className="mr-3 hover:opacity-70"
+            />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/martinamancuso"
+          >
+            <Image
+              src={githubIcon}
+              alt="github logo"
+              className="hover:opacity-70"
+            />
+          </a>
+        </div>
+        <div>
+          <p className="tracking-wider hidden lg:block">
+            Made with <span className="text-primary">❤</span>
+            <br />
+            with Next.js & Tailwind
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
