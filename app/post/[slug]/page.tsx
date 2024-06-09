fdd33a245ce7b73821990a88382f9709a7c0e6ad
martinamancuso <martinamancuso24@gmail.com>
2024-06-09 10:51:59 +0200

    feat: get and render blog post text

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
 export async function generateStaticParams() {
   const response = await fetch(
     `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT}/entries`,
@@ -46,60 +48,7 @@ export default async function Post({
             {post.fields.title}
           </div>
           <div className="text-sm text-greyText tracking-wide leading-loose text-justify flex flex-col gap-5">
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Temporibus recusandae amet tenetur enim modi officiis ex. Eligendi
-              assumenda doloribus voluptatibus eius doloremque? Possimus,
-              consequuntur. Ab eaque quasi repellendus quos dolorum! Explicabo
-              nesciunt ipsa debitis, ab aspernatur, consectetur cumque nobis
-              repudiandae ipsam officia architecto quis, labore fuga vero nisi
-              cupiditate impedit dolores aperiam. Dolorem repudiandae
-              praesentium impedit officiis, voluptatibus sit at! Maxime, id
-              soluta? Excepturi blanditiis illo est, consequuntur nulla,
-              provident accusamus doloremque sapiente, magnam voluptates fuga
-              porro modi dignissimos dolorem accusantium similique architecto
-              assumenda unde ipsam aspernatur eligendi ab deserunt? Similique
-              quidem pariatur est placeat accusamus excepturi doloribus eius
-              amet, mollitia impedit soluta iste exercitationem repudiandae
-              dolore recusandae corporis molestias ut quod vitae! Dicta laborum
-              alias optio dignissimos cupiditate dolore.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Temporibus recusandae amet tenetur enim modi officiis ex. Eligendi
-              assumenda doloribus voluptatibus eius doloremque? Possimus,
-              consequuntur. Ab eaque quasi repellendus quos dolorum! Explicabo
-              nesciunt ipsa debitis, ab aspernatur, consectetur cumque nobis
-              repudiandae ipsam officia architecto quis, labore fuga vero nisi
-              cupiditate impedit dolores aperiam. Dolorem repudiandae
-              praesentium impedit officiis, voluptatibus sit at! Maxime, id
-              soluta? Excepturi blanditiis illo est, consequuntur nulla,
-              provident accusamus doloremque sapiente, magnam voluptates fuga
-              porro modi dignissimos dolorem accusantium similique architecto
-              assumenda unde ipsam aspernatur eligendi ab deserunt? Similique
-              quidem pariatur est placeat accusamus excepturi doloribus eius
-              amet, mollitia impedit soluta iste exercitationem repudiandae
-              dolore recusandae corporis molestias ut quod vitae! Dicta laborum
-              alias optio dignissimos cupiditate dolore.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Temporibus recusandae amet tenetur enim modi officiis ex. Eligendi
-              assumenda doloribus voluptatibus eius doloremque? Possimus,
-              consequuntur. Ab eaque quasi repellendus quos dolorum! Explicabo
-              nesciunt ipsa debitis, ab aspernatur, consectetur cumque nobis
-              repudiandae ipsam officia architecto quis, labore fuga vero nisi
-              cupiditate impedit dolores aperiam. Dolorem repudiandae
-              praesentium impedit officiis, voluptatibus sit at! Maxime, id
-              soluta? Excepturi blanditiis illo est, consequuntur nulla,
-              provident accusamus doloremque sapiente, magnam voluptates fuga
-              porro modi dignissimos dolorem accusantium similique architecto
-              assumenda unde ipsam aspernatur eligendi ab deserunt? Similique
-              quidem pariatur est placeat accusamus excepturi doloribus eius
-              amet, mollitia impedit soluta iste exercitationem repudiandae
-              dolore recusandae corporis molestias ut quod vitae! Dicta laborum
-              alias optio dignissimos cupiditate dolore.
-            </p>
+            {documentToReactComponents(post.fields.content)}
           </div>
         </div>
       </div>
