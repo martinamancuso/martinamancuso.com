89cd00d038ecd86b976e1bf2764f1282d60a0309
martinamancuso <martinamancuso24@gmail.com>
2024-06-09 10:45:21 +0200

    feat: get image, date and title for a single blog post

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,16 +1,49 @@
-export default function Post() {
+export async function generateStaticParams() {
+  const response = await fetch(
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT}/entries`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      },
+    }
+  );
+  const data = await response.json();
+  const posts = data.items;
+  return posts.map((post: any) => ({
+    slug: post.fields.slug,
+  }));
+}
+
+export default async function Post({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const response = await fetch(
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT}/entries?content_type=blogPost&fields.slug=${params.slug}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      },
+    }
+  );
+  const data = await response.json();
+  const post = data.items[0];
+  const assets = data.includes.Asset;
+  const dateObject = new Date(post.sys.createdAt);
+
   return (
     <div className="page-container">
       <div className="container">
         <img
-          src="https://picsum.photos/600/400"
+          src={assets[0].fields.file.url}
           alt="Lorem Ipsum"
           className="w-full h-auto object-cover pb-9"
         />
         <div>
-          <div className="pb-5 text-sm text-greyText">24 Jun 2024</div>
+          <div className="pb-5 text-sm text-greyText">{dateObject.toLocaleDateString()}</div>
           <div className="py-5 border-t border-greyBorders text-2xl font-semibold">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {post.fields.title}
           </div>
           <div className="text-sm text-greyText tracking-wide leading-loose text-justify flex flex-col gap-5">
             <p>
