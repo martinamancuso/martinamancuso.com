8587eb0339e48ad0daa961aa2429df63d09ff59a
martinamancuso <martinamancuso24@gmail.com>
2024-06-23 14:24:39 +0200

    fix: remove object-cover and fix image height in blog list page

--- app/blog/blog-card.tsx
+++ app/blog/blog-card.tsx
@@ -14,7 +14,7 @@ export function BlogCard({
 }) {
   return (
       <Link href={path} className="bg-white shadow grow shrink-0 basis-2/5">
-        <img src={image} alt={title} className="w-full h-64 object-cover" />
+        <img src={image} alt={title} className="w-full h-auto" />
         <div className="p-7">
           <div className="pb-2"><FormattedDate date={date} /></div>
           <div className="pt-2 border-t border-greyBorders text-lg font-semibold">
